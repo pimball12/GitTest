c41854769654104b04619a0192442fdf419ecec4
Neto Mella <mella.neto@gmail.com>
2014-10-29 11:03:38 -0200

    Criacao da primeira versao da classe fatorial

---
+++ Fatorial.java
@@ -0,0 +1,17 @@
+public class Fatorial	{
+	
+	public static void main(String[] args)	{
+		System.out.println(CalcularFatorial(5));
+	}
+	
+	public static int CalcularFatorial(int n)	{
+		int resultado = 1;
+
+		for (int i = 2; i <= n; i++)	{
+			resultado*= i;
+		}
+
+		return resultado;
+	}
+
+}
