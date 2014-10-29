021e72742c85b23f2ebdb5c6db3ea1b9a74043ec
Neto Mella <mella.neto@gmail.com>
2014-10-29 11:23:23 -0200

    Foi adicionado um Hello, World! no fatorial.java

--- Fatorial.java
+++ Fatorial.java
@@ -2,6 +2,7 @@ public class Fatorial	{
 	
 	public static void main(String[] args)	{
 		System.out.println(CalcularFatorial(5));
+		System.out.println("Hello, world!");
 	}
 	
 	public static int CalcularFatorial(int n)	{
