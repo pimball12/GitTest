ff57ea661f22f5e7744c5a5c618ec223c204628d
Neto Mella <mella.neto@gmail.com>
2014-10-30 08:31:19 -0200

    Alteracao para lancar excessao em caso de numeros negativos

--- Fatorial.java
+++ Fatorial.java
@@ -1,10 +1,14 @@
 public class Fatorial	{
 	//Versao iterativa do calculo do fatorial.
-	public int calcularFatorial(int n)	{
+	public int calcularFatorial(int n) throws IllegalArgumentException	{
+		if (n<0)	{
+			throw new  IllegalArgumentException("Não existe fatorial para negativo.");
+		}
 		int resultado = 1;
-
-		for (int i = 2; i <= n; i++)	{
-			resultado*= i;
+		if (n>0)	{
+			for (int i = 2; i <= n; i++)	{
+				resultado*= i;
+			}
 		}
 
 		return resultado;
